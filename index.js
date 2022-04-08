731b6ea4c8a11c7ee03a86b35872953090f3defd
Brian7950 <brianh7950@gmail.com>
2022-04-08 15:37:08 -0600

    intern/manager prompt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const inquirer = require('inquirer');
 const fs = require('fs');
+const htmlTemp = "";
 
 function init(){
     inquirer.prompt([
@@ -31,5 +32,76 @@ function init(){
 
 
 function getManagerDetails(){
-    inquirer.prompt()
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter Manager name:",
+            name:"name"
+        },
+        {
+            type:"input",
+            message:"Enter Manager ID:",
+            name:"id"
+        },
+        {
+            type:"input",
+            message:"Enter Manager Email:",
+            name:"email"
+        },
+        {
+            type:"input",
+            message:"Enter Office number:",
+            name:"officeNumber"
+        }
+    ]).then(({name,id,email,officeNumber}) => {
+        const managerObj = new Manager(name, id, email, officeNumber) 
+        htmlTemp += ` <div class="col">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5 class="card-title">${managerObj.name} title</h5>
+            <p class="card-text">${managerObj.email}</p>
+            <p class="card-text">${managerObj.id}</p>
+            <p class="card-text">${managerObj.officeNumber}</p>
+          </div>
+        </div>
+      </div>`
+    })
 }
+
+function getInternDetails(){
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter Intern name:",
+            name:"name"
+        },
+        {
+            type:"input",
+            message:"Enter Intern ID:",
+            name:"id"
+        },
+        {
+            type:"input",
+            message:"Enter Intern Email:",
+            name:"email"
+        },
+        {
+            type:"input",
+            message:"Enter School name:",
+            name:"school"
+        }
+    ]).then(({name,id,email,school}) => {
+        const internObj = new Intern(name, id, email, school) 
+        htmlTemp += ` <div class="col">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5 class="card-title">${internObj.name} title</h5>
+            <p class="card-text">${internObj.email}</p>
+            <p class="card-text">${internObj.id}</p>
+            <p class="card-text">${internObj.school}</p>
+          </div>
+        </div>
+      </div>`
+    })
+}
+

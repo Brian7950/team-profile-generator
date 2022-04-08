c511ffc4c915bf709e052e9f4d563acdc8839732
Brian7950 <brianh7950@gmail.com>
2022-04-08 15:17:31 -0600

    inquirer starting questions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,38 @@
-const { test, expect } = require("@jest/globals");
+const Employee = require("../lib/Employee");
 
-test('creates a player object', () =>{
-    const employee = new Employee('test');
+test('checking name property', () =>{
+    const employee = new Employee('Brian');
 
-    expect(employee.name).toBe('test');
-    expect
+    expect(employee.name).toBe('Brian');
 })
+
+test('checking for id', () =>{
+    const employee = new Employee('Brian', 1);
+
+    expect(employee.id).toBe(1);
+})
+
+test('checking for email', () =>{
+    const employee = new Employee('Brian', 1, "Brian@something.com");
+
+    expect(employee.email).toBe("Brian@something.com");
+})
+
+test('checking getName method to return name', () =>{
+    const employee = new Employee('Brian');
+
+    expect(employee.getName()).toBe('Brian');
+})
+
+test('checking getId method to return id', () =>{
+    const employee = new Employee('Brian', 1);
+
+    expect(employee.getId()).toBe(1);
+})
+
+test('checking getEmail method to return email', () =>{
+    const employee = new Employee('Brian', 1, "Brian@something.com");
+
+    expect(employee.getEmail()).toBe("Brian@something.com");
+})
+
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+function init(){
+    inquirer.prompt([
+        {
+            type:"list",
+            choices: ["add Manager", "add Intern", "add Engineer", "Exit application"],
+            message: "What would you like to do?",
+            name: "userChoice"
+        }
+    ]).then(({userChoice}) =>{
+        switch(userChoice){
+            case"add Manager":
+                getManagerDetails();
+                break;
+            case"add Intern":
+                getInternDetails();
+                break;
+            case"add Engineer":
+                getEngineerDetails();
+                break;
+            default: 
+                createPage();
+        }
+    })
+}
+
+
+function getManagerDetails(){
+    inquirer.prompt()
+}
